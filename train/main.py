85566f2e5e9f751224c7143d0243014f3c5b4d65
suyukun <suyukun666@outlook.com>
2019-03-04 19:22:36 +0800

    debug

--- train/main.py
+++ train/main.py
@@ -100,9 +100,10 @@ for p in my_net.parameters():
 
 for epoch in range(n_epoch):
 
-    len_dataloader = min(len(dataloader_source), len(dataloader_target))
+    # len_dataloader = min(len(dataloader_source), len(dataloader_target))
+    len_dataloader = len(dataloader_source)
     data_source_iter = iter(dataloader_source)
-    data_target_iter = iter(dataloader_target)
+    # data_target_iter = iter(dataloader_target)
 
     i = 0
     while i < len_dataloader:
@@ -119,59 +120,66 @@ for epoch in range(n_epoch):
 
         input_img = torch.FloatTensor(batch_size, 3, image_size, image_size)
         class_label = torch.FloatTensor(batch_size)
-        domain_label = torch.zeros(batch_size)
-        domain_label = domain_label.long()
+        # domain_label = torch.zeros(batch_size)
+        # domain_label = domain_label.long()
 
         if cuda:
             s_img = s_img.cuda()
             s_label = s_label.cuda()
             input_img = input_img.cuda()
             class_label = class_label.cuda()
-            domain_label = domain_label.cuda()
+            # domain_label = domain_label.cuda()
 
         input_img.resize_as_(s_img).copy_(s_img)
         class_label.resize_as_(s_label).copy_(s_label)
         inputv_img = Variable(input_img)
         classv_label = Variable(class_label)
-        domainv_label = Variable(domain_label)
+        # domainv_label = Variable(domain_label)
 
         class_output, domain_output = my_net(input_data=inputv_img, alpha=alpha)
         err_s_label = loss_class(class_output, classv_label)
-        err_s_domain = loss_domain(domain_output, domainv_label)
+        # err_s_domain = loss_domain(domain_output, domainv_label)
 
-        # training model using target data
-        data_target = data_target_iter.next()
-        t_img, _ = data_target
+        # # training model using target data
+        # data_target = data_target_iter.next()
+        # t_img, _ = data_target
 
-        batch_size = len(t_img)
+        # batch_size = len(t_img)
 
-        input_img = torch.FloatTensor(batch_size, 3, image_size, image_size)
-        domain_label = torch.ones(batch_size)
-        domain_label = domain_label.long()
+        # input_img = torch.FloatTensor(batch_size, 3, image_size, image_size)
+        # domain_label = torch.ones(batch_size)
+        # domain_label = domain_label.long()
 
-        if cuda:
-            t_img = t_img.cuda()
-            input_img = input_img.cuda()
-            domain_label = domain_label.cuda()
+        # if cuda:
+        #     t_img = t_img.cuda()
+        #     input_img = input_img.cuda()
+        #     domain_label = domain_label.cuda()
 
-        input_img.resize_as_(t_img).copy_(t_img)
-        inputv_img = Variable(input_img)
-        domainv_label = Variable(domain_label)
+        # input_img.resize_as_(t_img).copy_(t_img)
+        # inputv_img = Variable(input_img)
+        # domainv_label = Variable(domain_label)
 
-        _, domain_output = my_net(input_data=inputv_img, alpha=alpha)
-        err_t_domain = loss_domain(domain_output, domainv_label)
-        err = err_t_domain + err_s_domain + err_s_label
+        # _, domain_output = my_net(input_data=inputv_img, alpha=alpha)
+        # err_t_domain = loss_domain(domain_output, domainv_label)
+        # err = err_t_domain + err_s_domain + err_s_label
+        # err.backward()
+        # optimizer.step()
+
+        err = err_s_label
         err.backward()
         optimizer.step()
 
         i += 1
 
-        print ('epoch: %d, [iter: %d / all %d], err_s_label: %f, err_s_domain: %f, err_t_domain: %f' \
-              % (epoch, i, len_dataloader, err_s_label.cpu().data.numpy(),
-                 err_s_domain.cpu().data.numpy(), err_t_domain.cpu().data.numpy()))
+        # print ('epoch: %d, [iter: %d / all %d], err_s_label: %f, err_s_domain: %f, err_t_domain: %f' \
+        #       % (epoch, i, len_dataloader, err_s_label.cpu().data.numpy(),
+        #          err_s_domain.cpu().data.numpy(), err_t_domain.cpu().data.numpy()))
+        
+        print ('epoch: %d, [iter: %d / all %d], err_s_label: %f' \
+              % (epoch, i, len_dataloader, err_s_label.cpu().data.numpy()))
 
     torch.save(my_net, '{0}/VOC_model_epoch_{1}.pth'.format(model_root, epoch))
     test(source_dataset_name, epoch)
-    test(target_dataset_name, epoch)
+    # test(target_dataset_name, epoch)
 
 print ('done')
